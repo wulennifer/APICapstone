9e3e815c7133af810193555d11df022967ebe6b7
Lennifer Wu <lennifer.wu@gmail.com>
2018-04-30 23:36:34 -0500

    added functions to get PF API data and initMap() working

--- index.js
+++ index.js
@@ -1,45 +1,104 @@
-'use strict';
+const PF_ENDPOINT = 'https://api.petfinder.com/';
+const PF_KEY = '21932220ec6653a1bd95d471bf57a22a';
+const GM_KEY = 'AIzaSyBF7QvTB30q44YdyF3DTGQMaMfZZfIWhr0';
 
-const PF_ENDPOINT = 'http://api.petfinder.com/';
-const GM_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
-const YELP_ENDPOINT = 'https://api.yelp.com/v3/businesses/search';
-const WIKI_ENDPOINT ='https://en.wikipedia.org/w/api.php';
-
-let map;
 let zipcode;
+let map;
 
-// Initiate Google Maps to 'map' <div>
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.397, lng: 150.644},
-  		zoom: 15,
-  		zoomControl: true,
-  		zoomControlOptions: {
-  			position: google.maps.ControlPosition.LEFT_CENTER;
-  		},
+		center: {lat: 41.881832, lng: -87.6298},
+  		zoom: 10,
+  		zoomControl: true
 	});
 }
 
-// Retrieve zipcode from User and then initiate map based off of zipcode
 function watchLocationSubmit() {
-	const locationForm = $('form[name=zipcode-search]');
-	const zipcodeField = $('input[name=zipcode]');
 
-	locationForm.on('submit', function(e)) {
+	$('.search').on('click', '.submitButton', function(e) {
+	  console.log("submitButton clicked");
 		e.preventDefault();
+		/*$('.landing-page').hide();*/
+		/*$('.js-output').show();*/
+		
 		// get zipcode
-		zipcode = zipcodeField.val();
-		// pass it to Google Maps endpoint
-		fetchPFNearbyRescData(GM_ENDPOINT, zipcode);
+		zipcode = $('input[name=zipcode]').val();
+		console.log("Zip entered: " + zipcode);
 		// reset input field
-		zipcodeField.val('');
-	}
+		$('input[name=zipcode]').val('');
+		getShelters(zipcode, showShelters);
+	});
 }
 
-// Create complete URL by adding zipcode to baseURL
-function fetchPFNearbyRescData(baseURL, zipcode) {
+function getShelters(zipcode, callback) {
+	let method = 'shelter.find';
+	let url = PF_ENDPOINT + method;
+	console.log("url: " + url);
+
+  $.ajax({
+			url: url,
+			jsonp: 'callback',
+			dataType: 'jsonp',
+			data: {
+				key: PF_KEY,
+				'location': zipcode,
+				count: 10,
+				output: 'basic',
+				format: 'json'
+			},
+			success: callback
+			
+			/*function(response) {
+			  console.log("success");
+			}*/,
+			error: function(xhr){
+        console.log('Request Status: ' + xhr.status + ' Status Text: ' + xhr.statusText + ' ' + xhr.responseText);
+			}
+	});
 }
 
-// Given the zipcode, display all nearby rescues
-function showRescues(zipcode) {
+function showShelters(data) {
+  console.log("data is: " + JSON.stringify(data, null, 4));
+
+  //loop through each shelter returned
+/*  for (let i = 0; i < data.petfinder.shelters.shelter.length; i++) {
+    let lat = data.petfinder.shelters.shelter[i].latitude.$t;
+  	  console.log("Lat is: " + lat);
+  	let lng = data.petfinder.shelters.shelter[i].longitude.$t;
+  	  console.log("Lng is: " + lng);
+  	let shelterId = data.petfinder.shelters.shelter[i].id.$t;
+  	  console.log("shelterId is: " + shelterId);
+  	let shelterName = data.petfinder.shelters.shelter[i].name.$t;
+  	  console.log("shelterName is: " + shelterName);
+  	let shelterEmail = data.petfinder.shelters.shelter[i].email.$t;
+  	  console.log("shelterEmail is: " + shelterEmail);
+  	let shelterPhone = data.petfinder.shelters.shelter[i].phone.$t;
+  	  console.log("shelterPhone is: " + shelterPhone);
+  }*/
+  
+  data.petfinder.shelters.shelter.forEach((shltr,index) => {
+    $('.shelter-results').append(renderShelter(shltr));
+  });
+
 }
+
+function renderShelter(shltr) {
+	return `
+	<div class='shelter-container'>
+		<h2 id='shelter-name'>${shltr.name.$t}</h2>
+		<h2 id='shelter-address'>${shltr.address1.$t} 
+			${shltr.city.$t}
+			${shltr.state.$t}
+			${shltr.zip.$t}
+			${shltr.country.$t}</h2>
+		<h2 id='shelter-phone'>${shltr.phone.$t}</h2>
+		<a href='mailto:${shltr.email.$t}' id='shelter-email'>${shltr.email.$t}</a>
+	</div>
+	`;
+}
+
+function nothingToShow() {
+  
+}
+
+watchLocationSubmit();
